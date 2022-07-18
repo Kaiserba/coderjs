dd799231e820582cdbd87db2154a11e6d59b3e3e
Gabriel Cortez <jgabrielcortezz@gmail.com>
2022-07-18 13:28:28 -0300

    descuento js

---
+++ descuento.js
@@ -0,0 +1,24 @@
+function descuento() {
+
+ let ingresarNumero = prompt("Ingrese un numero: ")
+ let ingresarDescuento = prompt("Ingresar descuento: ")
+
+ resultado = (ingresarNumero * (ingresarDescuento /100))
+
+ alert("El precio final es de :" + ( ingresarNumero - resultado))
+
+}
+
+descuento();
+
+
+
+// function solicitarNombre() {
+//     let nobreIngresado = prompt("ingresar nombre")
+
+//     alert("el nombre es " + nobreIngresado)
+    
+// }
+
+
+
