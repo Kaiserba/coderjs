cdb25b020f0105ab80176e4e8557e552871d04f7
Gabriel Cortez <jgabrielcortezz@gmail.com>
2022-07-25 17:14:27 -0300

    usar id de precio

--- descuentofinal.js
+++ descuentofinal.js
@@ -1,31 +1,6 @@
-
-
 function descuento() {
 
-
-
-var arrayofObjects = [
-    { id:"1" ,name:"Remera manga corta", precio: "$600" },
-    { id:"2" ,name:"Remera manga larga", precio: "$800" },
-    { id:"3" ,name:"Choba", precio: "$1500" },
-    { id:"4" ,name:"Camisa", precio: "$2300" },
-]
-
-let productoID = prompt("Que producto desea? \n 1) Remera manga corta \n 2) Remera manga larga \n 3) Chomba \n 4) Camisa")
-
-arrayofObjects.forEach(object =>{
-    if(object.id === productoID){
-    alert("Producto : " + object.name + "\n" + "Precio Prodcuto :" + object.precio);
-    }
-
-
-
-});
-
-    
-
-    let ingresarNumero = arrayofObjects["precio"]
-    
+    let ingresarNumero = parseInt(prompt("Ingrese el monto del producto: "))
 
     let cuotas = prompt('Elija la opcion deseada \n  1) 3 cuotas un interes del 15 % \n  2) 6 cuotas un interes del 20 ')
 
