14a73de0da970f9a9a03024434348c1bdad59558
Juan gabriel <jgabrielcortezz@gmail.com>
2022-07-26 20:45:46 -0300

    array entregar1

--- arryaproductos.js
+++ arryaproductos.js
@@ -2,39 +2,44 @@
 
 function descuento() {
 
+// array de productos y precios
 
-const arrayofObjects = [
+const productos = [
     { id:"1" ,name:"Remera manga corta", precio: "600" },
     { id:"2" ,name:"Remera manga larga", precio: "800" },
     { id:"3" ,name:"Choba", precio: "1500" },
     { id:"4" ,name:"Camisa", precio: "2300" },
 ]
 
+
+
 let productoID = prompt("Que producto desea? \n 1) Remera manga corta \n 2) Remera manga larga \n 3) Chomba \n 4) Camisa")
 
-arrayofObjects.forEach(object =>{
-    if(object.id === productoID){
-    alert("Producto : " + object.name + "\n" + "Precio Prodcuto :" + object.precio);
+productos.forEach(objecto =>{
+    if(objecto.id === productoID){     
+    alert("Producto : " + objecto.name + "\n" + "Precio Prodcuto :" + objecto.precio);
 
 
+let precioProducto =  parseFloat(objecto.precio)
 
 
-    
-    
+  alert('precio es '+ precioProducto)
 
-    let cuotas = prompt('Elija la opcion deseada \n  1) 3 cuotas un interes del 15 % \n  2) 6 cuotas un interes del 20 ')
+// aca empieza la forma de pago y descuento
+
+let cuotas = prompt('Elija la opcion deseada \n  1) 3 cuotas un interes del 10 % \n  2) 6 cuotas un interes del 20 % ')
 
     
  if (cuotas == 1) {
-    resultadoInteres = (ingresarNumero * 0.10)
-    resultadoFianlInteres = resultadoInteres + ingresarNumero
+    resultadoInteres = (precioProducto * 0.10)
+    resultadoFianlInteres = resultadoInteres + precioProducto
 
     alert('Sera en 3 cuotas y el precio final sera de ' + resultadoFianlInteres)
  }  else  
  
  if (cuotas == 2) {
-    resultadoInteres = (ingresarNumero * 0.20)
-    resultadoFianlInteres = resultadoInteres + ingresarNumero
+    resultadoInteres = (precioProducto * 0.20)
+    resultadoFianlInteres = resultadoInteres + precioProducto
 
     alert('Sera en 6 cuotas y el precio final sera de ' + resultadoFianlInteres)
  } else {
@@ -53,7 +58,10 @@ let opcion = prompt('Buenos días Ingerese la opcion deseada : \n 1) (10% de des
      }  else  {
         if (opcion == 3) {
             resultadoDescuento = resultadoFianlInteres * 0.25
-
+         
+    } else {
+        alert("no se aplicara nigun descuento:  \n El precio final a pagar es de : " + resultadoFianlInteres )
+    }
     
      }
   
@@ -63,7 +71,12 @@ let opcion = prompt('Buenos días Ingerese la opcion deseada : \n 1) (10% de des
 
  alert("El precio final es de :" + (resultadoFianlInteres - resultadoDescuento) )
 
+
+
 }
+})
+
+};
 
 
 descuento();
