3df9bdcad35e7afb2fb0ea2f777acb38d2f545dd
Juan gabriel <jgabrielcortezz@gmail.com>
2022-08-11 18:31:51 -0300

    retomamos

--- carrito1.1/app.js
+++ carrito1.1/app.js
@@ -1,16 +1,19 @@
 const cards = document.getElementById('cards')
-const productos = document.getElementById('producto')
+const items = document.getElementById('items')
 const footer = document.getElementById('footer')
 const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
 const fragment = document.createDocumentFragment()
 let carrito = {}
 
-
+// Eventos
+// El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
 document.addEventListener('DOMContentLoaded', e => { fetchData() });
 cards.addEventListener('click', e => { addCarrito(e) });
 items.addEventListener('click', e => { btnAumentarDisminuir(e) })
 
-// llamar productos
+// Traer productos
 const fetchData = async () => {
     const res = await fetch('api.json');
     const data = await res.json()
@@ -18,23 +21,39 @@ const fetchData = async () => {
     pintarCards(data)
 }
 
-    // crear cards
+// Pintar productos
 const pintarCards = data => {
     data.forEach(producto => {
         templateCard.querySelector('h5').textContent = producto.title
         templateCard.querySelector('p').textContent = producto.precio
-        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
+        templateCard.querySelector('img').setAttribute('src', producto.thumbnailUrl)
         templateCard.querySelector('button').dataset.id = producto.id
-        
-
-       
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     })
     cards.appendChild(fragment)
 }
 
+// Agregar al carrito
+const addCarrito = e => {
+    if (e.target.classList.contains('btn-dark')) {
+        // console.log(e.target.dataset.id)
+        // console.log(e.target.parentElement)
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
+
+const setCarrito = item => {
+   
+    const producto = {
+        id: item.querySelector('button').dataset.id,
+   
+    }
+
+    console.log(producto)
 
 
+}
 
 
